f0e907967a2ace77bb325204006b90df9a4d9f15
Ganesan-P <ganesanganapathi92@gmail.com>
2024-12-18 16:22:01 +0530

    bracket checker

---
+++ first.py
@@ -0,0 +1,13 @@
+class check_brackets:
+    s=[]
+    dict={'(':')','[':']','{':'}'}
+    def __init__(data):
+        for i in dict:
+            if i in data:
+                i.push(s)
+            else:
+                s(i.pop())
+                return False
+        return True
+s1=check_brackets()
+s1.check_brackets('{[]}')
